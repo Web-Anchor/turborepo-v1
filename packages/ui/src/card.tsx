6c5dba70938e060d40888c67f949b2e431c8b07d
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-07-23 12:36:13 +0100

    New commit message: snags to card & add root tsconfig

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode } from "react";
+import { type ReactNode } from 'react';
 
 export function Card({
   title,
@@ -16,8 +16,8 @@ export function Card({
       rel="noopener noreferrer"
       target="_blank"
     >
-      <h2 className="ui-mb-3 ui-text-2xl ui-font-semibold">
-        {title}{" "}
+      <h2 className="ui-mb-3 ui-text-2xl ui-font-semibol ui-text-red-400">
+        {title}{' '}
         <span className="ui-inline-block ui-transition-transform group-hover:ui-translate-x-1 motion-reduce:ui-transform-none">
           -&gt;
         </span>
