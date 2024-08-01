3415bdc8f2b8aadb920fc838670b001db9b8121c
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-08-01 10:39:21 +0100

    New commit message: extend header section & implement theme types

--- packages/components/src/HeaderSection/index.tsx
+++ packages/components/src/HeaderSection/index.tsx
@@ -5,6 +5,7 @@ type HeaderSectionProps = {
   subtitle?: string;
   description?: string;
   className?: string;
+  theme?: 'light' | 'dark';
 };
 
 export function HeaderSection(props: HeaderSectionProps) {
@@ -12,17 +13,32 @@ export function HeaderSection(props: HeaderSectionProps) {
     <div className={classNames('px-6 py-24 sm:py-32 lg:px-8', props.className)}>
       <div className="mx-auto max-w-2xl text-center">
         {props.subtitle && (
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">
+          <h2
+            className={classNames(
+              'text-base font-semibold leading-7 text-indigo-600',
+              props.theme === 'dark' && 'text-gray-400'
+            )}
+          >
             {props.subtitle}
           </h2>
         )}
         {props.title && (
-          <h1 className="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          <h1
+            className={classNames(
+              'mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl',
+              props.theme === 'dark' && 'text-white'
+            )}
+          >
             {props.title}
           </h1>
         )}
         {props.description && (
-          <p className="mt-6 text-lg leading-8 text-gray-600">
+          <p
+            className={classNames(
+              'mt-6 text-lg leading-8 text-gray-600',
+              props.theme === 'dark' && 'text-gray-400'
+            )}
+          >
             {props.description}
           </p>
         )}
