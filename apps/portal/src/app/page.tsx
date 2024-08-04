8f5d8f493d9ebbc256719538a01213b8c4ae866e
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-08-04 16:16:00 +0100

    New commit message: add platform section & snags

--- apps/portal/src/app/page.tsx
+++ apps/portal/src/app/page.tsx
@@ -78,6 +78,21 @@ export default function Home() {
     <Wrapper className="h-full">
       <Header />
 
+      <HeaderSection
+        title="Welcome to Your Invoicing Portal"
+        description={[
+          'Discover the ease of managing your financial activities from one central location. Our platform offers you a dedicated space where you can effortlessly view transactions from your vendor, download invoices, and make necessary adjustments such as adding notes or updating details. With these powerful features, you can keep track of all your financial dealings and ensure that your records are always up-to-date and accurate. Contact your vendor today to access your personalized invoicing portal and start experiencing the convenience of streamlined financial management.',
+        ]}
+        subtitle="Your Gateway to Effortless Invoicing!"
+      />
+      <HeaderSection
+        title="Customer Portal. Your Invoicing made smarter."
+        subtitle="Empowering You, One Click at a Time!"
+        description={[
+          'Experience the next level of efficiency and convenience with our Customer Portal. Building on the ease of managing your financial activities, our platform allows you to effortlessly handle invoicing, download essential documents, and track transactions with precision. Our intuitive interface ensures that you can streamline your financial tasks and maintain control without any hassle. Join us today and see how simple and effective financial management can be with our tailored invoicing solutions.',
+        ]}
+        theme="dark"
+      />
       <ConnectSectionWithStickyImg
         header={{
           subtitle: 'Empowering Your Financial Success',
@@ -239,17 +254,8 @@ export default function Home() {
         ]}
       />
       <HeaderSection
-        title="Welcome to Your Invoicing Portal"
         description={[
-          'Discover the ease of managing your financial activities from one central location. Our platform offers you a dedicated space where you can effortlessly view transactions from your vendor, download invoices, and make necessary adjustments such as adding notes or updating details. With these powerful features, you can keep track of all your financial dealings and ensure that your records are always up-to-date and accurate. Contact your vendor today to access your personalized invoicing portal and start experiencing the convenience of streamlined financial management.',
-        ]}
-        subtitle="Your Gateway to Effortless Invoicing!"
-      />
-      <HeaderSection
-        title="Customer Portal. Your Invoicing made smarter."
-        subtitle="Empowering You, One Click at a Time!"
-        description={[
-          'Experience the next level of efficiency and convenience with our Customer Portal. Building on the ease of managing your financial activities, our platform allows you to effortlessly handle invoicing, download essential documents, and track transactions with precision. Our intuitive interface ensures that you can streamline your financial tasks and maintain control without any hassle. Join us today and see how simple and effective financial management can be with our tailored invoicing solutions.',
+          'Our platform is designed to provide you with a seamless and efficient invoicing experience. With our Customer Portal, you can easily manage your financial activities, view transactions, and download invoices with just a few clicks. Our user-friendly interface ensures that you can stay in control of your finances without any hassle. Join now to start enjoying the benefits of a streamlined invoicing process and experience the convenience of managing your financial activities in one central location.',
         ]}
         theme="dark"
       />
