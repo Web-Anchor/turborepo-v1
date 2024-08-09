257dbb263ea9c06f73c3132eef7cc2ca6e8d9bba
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-08-09 14:45:20 +0100

    New commit message: cta loading stae snags

--- packages/components/src/Button/index.tsx
+++ packages/components/src/Button/index.tsx
@@ -27,12 +27,20 @@ export function Button(props: Props): React.ReactElement | null {
     primary:
       'rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
     secondary:
-      'rounded-md bg-indigo-600 px-3 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+      'rounded-md bg-gray-800 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
     ghost:
       'bg-transparent inline-flex items-center border-b-2 border-transparent px-1 text-sm font-medium text-gray-500 hover:text-gray-700 hover:bg-transparent shadow-none',
     badge:
       'center relative inline-block select-none whitespace-nowrap rounded-lg bg-amber-500 text-white py-2 px-3.5 align-baseline font-sans text-xs font-bold uppercase leading-none',
     link: 'bg-transparent items-center px-0 text-sm font-semibold text-indigo-600 shadow-none hover:bg-transparent hover:text-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
+  };
+
+  const spinnerStyles = {
+    primary: 'border-indigo-600 border-l-[#ffffff50]',
+    secondary: 'border-indigo-600 border-l-[#ffffff50]',
+    link: 'border-gray-400 border-l-indigo-600',
+    ghost: 'border-gray-400 border-l-indigo-600',
+    badge: 'border-gray-400 border-l-indigo-600',
   };
 
   function onClickHandler() {
@@ -54,8 +62,18 @@ export function Button(props: Props): React.ReactElement | null {
       data-cy={props.dataAttribute}
     >
       {props.fetching && (
-        <section>
-          <svg className={`animate-spin h-5 w-5`} viewBox="0 0 24 24"></svg>
+        <section
+          className={classNames(
+            'absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'
+          )}
+        >
+          <svg
+            className={classNames(
+              'animate-spin border-4 border-solid rounded-full h-5 w-5 bg-transparent',
+              spinnerStyles[props.style || 'primary']
+            )}
+            viewBox="0 0 24 24"
+          ></svg>
         </section>
       )}
       <section className="w-fill truncate mx-auto">
