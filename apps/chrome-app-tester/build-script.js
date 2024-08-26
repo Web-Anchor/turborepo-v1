b06add2304acb1b2653e5894254e22fda47b0a78
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-08-26 10:40:30 +0100

    New commit message: refactor & restructure chrome app tester app

--- apps/chrome-app-tester/build-script.js
+++ apps/chrome-app-tester/build-script.js
@@ -1,10 +1,16 @@
-import { copyFileSync } from 'fs';
+import { copyFileSync, mkdirSync } from 'fs';
 import { resolve } from 'path';
 
-const src = resolve('./manifest.json'); // Adjust the source path as needed
-const dest = resolve('dist/manifest.json');
-console.log(`🏋️‍♂️ Copying manifest.json from ${src} to ${dest}`);
+try {
+  const src = resolve('./workers/background.js'); // Adjust the source path as needed
+  const dest = resolve('./dist/workers/background.js');
+  console.log(`🏋️‍♂️ Copying manifest.json from ${src} to ${dest}`);
+  // create a new directory in the dist folder workers
+  mkdirSync(resolve('./dist/workers'), { recursive: true });
 
-copyFileSync(src, dest);
+  copyFileSync(src, dest);
 
-console.log(`✨ Copied manifest.json to dist folder`);
+  console.log(`✨ Copied manifest.json to dist folder`);
+} catch (error) {
+  console.error('Error copying manifest.json', error);
+}
